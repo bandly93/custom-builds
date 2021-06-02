305f3314f31ca1656585003cdd6d4765e0d904bc
Band Ly <bandly93@gmail.com>
2021-06-02 16:24:58 -0700

    Update ckeditor.js

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -86,7 +86,24 @@ Editor.defaultConfig = {
 			'imageStyle:side',
 			'|',
 			'imageTextAlternative'
-		]
+		],
+		resizeOptions: [
+                {
+                    name: 'resizeImage:original',
+                    label: 'Original',
+                    value: null
+                },
+                {
+                    name: 'resizeImage:50',
+                    label: '50%',
+                    value: '50'
+                },
+                {
+                    name: 'resizeImage:75',
+                    label: '75%',
+                    value: '75'
+                }
+            ],
 	},
 	table: {
 		contentToolbar: [
